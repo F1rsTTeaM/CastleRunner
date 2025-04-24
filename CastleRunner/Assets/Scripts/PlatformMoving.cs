a246126d9e1b7bc0366fe9667820af5eeb25f002
F1rsTTeaM <ladyagin2005@mail.ru>
2025-04-24 14:00:19 +0400

    Added Platform mechanics

---
+++ CastleRunner/Assets/Scripts/PlatformMoving.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlatformMoving : MonoBehaviour
+{
+    public float speed = 2f;
+    public Transform pointA;
+    public Transform pointB;
+
+    private Vector3 target;
+
+    void Start()
+    {
+        target = pointB.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target) < 0.1f)
+        {
+            if (target == pointA.position)
+            {
+                target = pointB.position;
+            }
+            else
+            {
+                target = pointA.position;
+            }
+        }
+    }
+}
